241be30bbfe61e9500818487db8310e3084704ad
hughbagan <25750667+hughbagan@users.noreply.github.com>
2020-08-13 16:12:01 -0600

    Define PY_SSIZE_T_CLEAN - Trying to fix the "DeprecationWarning: PY_SSIZE_T_CLEAN will be required for '#' formats"

--- src/bindings/python/pycsp.c
+++ src/bindings/python/pycsp.c
@@ -18,6 +18,8 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+#define PY_SSIZE_T_CLEAN // docs.python.org/3/c-api/intro.html#include-files
+
 #include <Python.h>
 #include <csp/csp.h>
 #include <csp/csp_cmp.h>
